2a12ce9e1f5be9523047175d092a3328f08a7fc6
Sebona Ifa <sebona@sebona.localdomain>
2024-05-22 00:25:50 +0300

    solidity commit

---
+++ SolidityIntroduction/unsignedintegers.sol
@@ -0,0 +1,5 @@
+contract Contract {
+    uint8 public a = 100; // example value between 0 and 255
+    uint16 public b = 256; // minimum value for uint16
+    uint256 public sum = a + b; // sum of a and b
+}
